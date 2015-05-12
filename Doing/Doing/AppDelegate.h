82563537b726a1b4b9a2b726c98a0019140cafb6
ClearAll <zhj710326527@163.com>
2015-05-12 10:35:09 +0800

    create project in Zane-Doing

---
+++ Doing/Doing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Doing
+//
+//  Created by 周红俊 on 15/5/12.
+//  Copyright (c) 2015年 Zane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
